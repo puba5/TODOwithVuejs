580e647c59feda6fe8fe62ca0804e09d52f3158c
WooYoo <puba5@naver.com>
2020-01-25 13:51:56 +0900

    add Animation

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # learn_vuejs_project
 
-> A Vue.js project
+> Make TODO app with Vue.js
 
 ## Build Setup
 
-``` bash
+```bash
 # install dependencies
 npm install
 
@@ -16,3 +16,8 @@ npm run build
 ```
 
 For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
+
+> 사용한 것들
+
+팝업 대화 상자를 위한 Modal
+더 나은 애니메이션을 위한 뷰-애니메이션
---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoFooterVue from "./components/TodoFooter.vue";
+import TodoListVue from "./components/TodoList.vue";
+import TodoInputVue from "./components/TodoInput.vue";
+import TodoHeaderVue from "./components/TodoHeader.vue";
+
+export default {
+  data() {
+    return {
+      todoItems: []
+    };
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+  methods: {
+    addTodo(todoItem) {
+      localStorage.setItem(todoItem, todoItem);
+      this.todoItems.push(todoItem);
+    },
+    clearAll() {
+      localStorage.clear();
+      this.todoItems = [];
+    },
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  },
+  components: {
+    TodoHeader: TodoHeaderVue,
+    TodoInput: TodoInputVue,
+    TodoList: TodoListVue,
+    TodoFooter: TodoFooterVue
+  }
+};
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: bisque;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
+
